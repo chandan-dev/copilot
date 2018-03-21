dc5f95a4add91ea18f75c3de6c5571a9b71b9887
Pravasini Sahoo <pravasinis@mindfiresolutions.com>
2018-03-21 10:59:29 +0530

    Added server DB details.

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "mindfire";
-$dbname = "copilot";
+//server details
+$servername = "sql312.epizy.com";
+$username = "epiz_21785510";
+$password = "chandan123";
+$dbname = "epiz_21785510_copilot";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,3 +12,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// localhost details
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "copilot";
+//
+//// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
